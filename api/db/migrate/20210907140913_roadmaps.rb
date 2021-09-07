3da44baa2c041576611575460462f4df050e7224
Kenjiro56 <hiroken506@gmail.com>
2021-09-07 23:11:04 +0900

    fature/roadmaps_table

---
+++ api/db/migrate/20210907140913_roadmaps.rb
@@ -0,0 +1,9 @@
+class Roadmaps < ActiveRecord::Migration[6.0]
+  def change
+    create_table :roadmaps do |t|
+      t.string :name,null: false
+      t.string :description,null: false
+      t.integer :user_id,null: false
+    end
+  end
+end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_07_140230) do
+ActiveRecord::Schema.define(version: 2021_09_07_140913) do
 
   create_table "NodeItems", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "description", null: false
     t.integer "next_id", null: false
     t.integer "roadmap_id", null: false
+  end
+
+  create_table "roadmaps", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "description", null: false
+    t.integer "user_id", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
