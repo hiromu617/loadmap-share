d4038d2c5b0ba016bda6d30c05b8e70d18ff74a9
Kenjiro56 <hiroken506@gmail.com>
2021-09-07 23:13:16 +0900

    feature/coments_table

---
+++ api/db/migrate/20210907141138_comments.rb
@@ -0,0 +1,9 @@
+class Comments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.integer :roadmap_id,null: false
+      t.integer :user_id,null: false
+      t.string :comment ,null: false
+    end
+  end
+end
