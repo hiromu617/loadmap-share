8800cf1f88203bf6cc29a7167282c4226439242e
hiro <hiromhiro617@gmail.com>
2021-09-19 22:19:49 +0900

    :sparkles: add login methos

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace 'api' do
     namespace 'v1' do
-      resources :users
+      resources :users do
+        collection do
+          get 'login', on: :collection
+        end
+      end
     end
   end
 end
