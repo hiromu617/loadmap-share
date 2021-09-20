35ac59c3f6faaa975e97b96da445e4c0fd04323d
hiro <hiromhiro617@gmail.com>
2021-09-20 22:08:39 +0900

    :sparkles: add anotate

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id            :bigint           not null, primary key
+#  uid           :string(255)      not null
+#  name          :string(255)      not null
+#  profile_image :string(255)
+#  bio           :string(255)      default(""), not null
+#
 class User < ApplicationRecord
 end
--- api/test/models/user_test.rb
+++ api/test/models/user_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id            :bigint           not null, primary key
+#  uid           :string(255)      not null
+#  name          :string(255)      not null
+#  profile_image :string(255)
+#  bio           :string(255)      default(""), not null
+#
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
