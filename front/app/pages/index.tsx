b99ab1063eb149e81186830dae2a52d033484c12
YusakuNokubi <saku.studyPG@gmail.com>
2021-09-20 13:13:52 +0900

    :art: #13
    データの取得は未実装

--- front/app/pages/index.tsx
+++ front/app/pages/index.tsx
@@ -6,13 +6,50 @@ import styles from "../styles/Home.module.css";
 
 // TODO: roadmap一覧
 const Home: NextPage = () => {
+  var roadMapList = new Array();
+  roadMapList.push(0);
+  roadMapList.push(1);
+  roadMapList.push(2);
+  roadMapList.push(3);
+  roadMapList.push(4);
+  roadMapList.push(5);
+  roadMapList.push(6);
+
   return (
-    <div>
-      {[0, 1, 2, 3, 4].map((id) => (
-        <Link key={id} href={`/roadmap/${id}`}>
-          <div className="bg-blue-500 w-20 h-10 mb-10">{id}</div>
-        </Link>
-      ))}
+    <div className="w-full h-full">
+      <div className="flex bottom-auto">
+        <div className="flex items-end text-4xl p-2 text-indigo-400">
+          Gallery
+        </div>
+        <div className="flex items-end p-2 text-gray-600">ロードマップ一覧</div>
+      </div>
+      <div className="border-solid p-10 flex flex-wrap content-center">
+        {roadMapList.map((id) => (
+          <Link key={id} href={`/roadmap/${id}`}>
+            <div className="w-1/3">
+              <div className="bg-gray-100 rounded-md mb-10 m-5 p-5">
+                <div className="font-bold text-gray-600">
+                  Unity実践入門～環境構築から
+                </div>
+                <div className="m-2 text-gray-600 ">
+                  Unityの環境構築から2D/3Dゲームの実装をするところ
+                  までのロードマップを提示しています。
+                </div>
+                <div className="flex right-0">
+                  <div className="flex items text-gray-600 ">アイコン</div>
+                  <div className="flex items text-gray-600 ">作成者</div>
+                  <div className="flex items">
+                    <div>
+                      <div className="text-gray-600 ">120</div>
+                      <div className="text-gray-600 ">2021.09.21</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
