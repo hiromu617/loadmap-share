3c4468dc724a3056baad74b2f19d2c2714fc3bce
hiro <hiromhiro617@gmail.com>
2021-09-25 11:59:22 +0900

    :sparkles: add api call in index.tsx

---
+++ front/app/src/types/RoadMap.ts
@@ -0,0 +1,23 @@
+export type RoadMap = {
+  id: number;
+  name: string;
+  description: string;
+  author: Author;
+  node_items: NodeItem[];
+  created_at: string;
+};
+
+export type Author = {
+  uid: string;
+  name: string;
+  bio: string;
+  profile_image: string;
+};
+
+export type NodeItem = {
+  id: number;
+  next_id: number;
+  loadmap_id: number;
+  name: string;
+  title: string;
+};
