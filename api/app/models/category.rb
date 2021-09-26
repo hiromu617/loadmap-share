4d527c0e216502dd9cb8a01c113ba42b1f85a7bb
ARUOHTA <48429644+ARUOHTA@users.noreply.github.com>
2021-09-26 20:44:47 +0900

    Update api/app/models/category.rb
    Co-authored-by: hiromu617 <64679766+hiromu617@users.noreply.github.com>

--- api/app/models/category.rb
+++ api/app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-    validates :name, presence: true, length: { maximum: 20 }
+    validates :name, presence: true, length: { maximum: 20 }, uniqueness: true
     has_many :roadmaps
 end
