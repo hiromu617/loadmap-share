ffce5e972db7123ce07063f80d4eefa91ae268a2
Kenjiro56 <hiroken506@gmail.com>
2021-09-26 17:34:40 +0900

    :rocket: add/development.rb
    developmentにhost.clearを追記

--- api/config/environments/development.rb
+++ api/config/environments/development.rb
@@ -46,6 +46,10 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
+  #https://qiita.com/ryota-0906/items/55c6204532988aa73c82 に従って追記
+  config.hosts.clear
+
+
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
